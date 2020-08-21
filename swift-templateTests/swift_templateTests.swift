a767e094134440925867bee613631d8fc84d5d5d
dianyi <lastobject@gmail.com>
2020-08-21 17:14:39 +1200

    init

---
+++ swift-templateTests/swift_templateTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_templateTests.swift
+//  swift-templateTests
+//
+//  Created by dianyi jiang on 21/08/20.
+//  Copyright © 2020 ORGNAME. All rights reserved.
+//
+
+import XCTest
+@testable import swift_template
+
+class swift_templateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
